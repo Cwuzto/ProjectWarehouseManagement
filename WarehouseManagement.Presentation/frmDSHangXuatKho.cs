2af4b5ee1f1f600f6f09a77a5b0e333104766c4f
Ng-Pham <nghipham2003@gmail.com>
2024-04-26 21:29:30 +0700

    update chuc nang cua quan ly

--- WarehouseManagement.Presentation/frmDSHangXuatKho.cs
+++ WarehouseManagement.Presentation/frmDSHangXuatKho.cs
@@ -76,6 +76,7 @@ namespace WarehouseManagement.Presentation
                 if (selectedItem == "Ngày xuất")
                 {
                     txtSearch.Visible = false;
+                    txtSearch.Clear();
                     dtpSearch.Visible = true;
                 }
                 else
