45437c02514e255618a401466f872a01ada7bebb
trameme <tram08667@gmail.com>
2024-05-03 21:39:05 +0700

    frmxemdshangton

---
+++ WarehouseManagement.Bussiness/XemDSHangtonBUS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagement.DataAccess;
+
+namespace WarehouseManagement.Bussiness
+{
+    public class XemDSHangtonBUS
+    {
+        XemDSHangton dsht = new XemDSHangton();
+        public DataTable Laydsht()
+        {
+            return dsht.GetAlldshangton();
+        }
+        public DataTable timhang(string data)
+        {
+            return dsht.timhanghoa(data);
+        }
+    }
+}
---
+++ WarehouseManagement.DataAccess/XemDSHangtonDL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManagement.DataAccess.Data;
+
+namespace WarehouseManagement.DataAccess
+{
+    public class XemDSHangton
+    {
+        DataTable dshtk;
+        public XemDSHangton()
+        {
+            var query = "SELECT * FROM [HangHoa]";
+            dshtk = DataProvider.Instance.ExecuteQuery(query);
+        }
+        public DataTable GetAlldshangton()
+        {
+            return dshtk;
+        }
+        public DataTable timhanghoa(string MaHH)
+        {
+            string query = "SELECT * FROM [HangHoa] WHERE MaHH LIKE @Keyword";
+            object[] parameter = { MaHH };
+            return DataProvider.Instance.ExecuteQuery(query, parameter);
+        }
+
+    }
+}
