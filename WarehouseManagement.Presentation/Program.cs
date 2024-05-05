10b29d2723d3a6b075eebf2e58520133df665e98
quynhnhutdm11 <qnhu11022004@gmail.com>
2024-05-05 14:46:10 +0700

    DDDD

--- WarehouseManagement.Presentation/Program.cs
+++ WarehouseManagement.Presentation/Program.cs
@@ -16,7 +16,7 @@ namespace WarehouseManagement.Presentation
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new FrmBaocao());
         }
     }
 }
